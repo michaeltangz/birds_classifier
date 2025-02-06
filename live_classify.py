9a91104a518b30afa1d188d939e357cefe57d0a3
Michael TANG <tongmichael16@gmail.com>
2025-02-06 14:51:41 +1300

    update map live_classify.py

--- live_classify.py
+++ live_classify.py
@@ -304,6 +304,107 @@ import requests
 #         st.write("Please enable location services and press the button to get your current location.")
 ##############################################################
 
+# import streamlit as st
+# import requests
+# from PIL import Image
+# from transformers import pipeline
+# import folium
+# from streamlit_folium import st_folium
+# from streamlit_geolocation import streamlit_geolocation
+
+# st.markdown("<h1 style='font-size: 20px;'>Welcome to Bird Identification Page</h1>", unsafe_allow_html=True)
+# st.write("This page will help you identify bird species and find the best nearest bird observation hotspots")
+# st.write("Please upload the image file of bird to identify the species")
+
+# # Create a file image uploader
+# uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+# if uploaded_file is not None:
+#     # Open the image using PIL
+#     img = Image.open(uploaded_file)
+
+#     # Display the uploaded image
+#     st.image(img, caption='Uploaded Image.', use_container_width=False, width=500)
+#     st.write("")
+#     st.write("⌛ Classifying......")
+
+#     # Loading the model and preprocessor using Pipeline
+#     pipe = pipeline("image-classification", model="dennisjooo/Birds-Classifier-EfficientNetB2")
+#     result = pipe(img)[0]
+#     st.write(f"**Prediction: {result['label']}**")
+#     label = result['label']
+
+#     def convert_label(label):
+#         return label.replace(" ", "_").lower()
+
+#     # Define the endpoint
+#     endpoint = "https://en.wikipedia.org/w/api.php"
+
+#     # Define the parameters
+#     params = {
+#         "action": "query",
+#         "format": "json",
+#         "titles": f"{convert_label(label)}",
+#         "prop": "extracts|pageimages",
+#         "exintro": True,
+#         "explaintext": True,
+#         "pithumbsize": 400
+#     }
+
+#     # Make the request
+#     response = requests.get(endpoint, params=params)
+
+#     # Check if the response is empty
+#     if response.text.strip() == "":
+#         st.write("Received empty response from Wikipedia API")
+#     else:
+#         try:
+#             # Parse the response
+#             data = response.json()
+
+#             # Extract the page content
+#             page = next(iter(data['query']['pages'].values()))
+#             extract = page.get('extract', 'No extract available')
+#             thumbnail = page.get('thumbnail', {}).get('source', None)
+
+#             # Display the image if available
+#             if thumbnail:
+#                 st.image(thumbnail)
+
+#             # Display the description
+#             st.write(f"🐤 **Description:** {' '.join(extract.split('.')[:3])}.")
+
+#         except requests.exceptions.RequestException as e:
+#             st.write("Failed to retrieve data from Wikipedia API")
+#             st.write(f"Error: {e}")
+#         except ValueError as e:
+#             st.write("Failed to parse JSON response from Wikipedia API")
+#             st.write(f"Error: {e}")
+
+#     st.write("⬇️ Please press the button to get your current location")
+
+
+
+# location = streamlit_geolocation()
+# if location and "latitude" in location and "longitude" in location:
+
+#     lat, lon = location["latitude"], location["longitude"]
+#     st.write(f"Your location is: {lat}, {lon}")
+
+#     # Create a map centered at the current location
+#     m = folium.Map(location=[lat, lon], zoom_start=12)
+
+#     folium.Marker(
+#         [lat, lon], 
+#         popup="Current Location", 
+#         icon=folium.Icon(color='red')  # Set marker color
+#     ).add_to(m)
+
+#     st_folium(m, width=600, height=300)
+# else:
+#     st.write("Please enable location services and press the button to get your current location.")
+
+
 import streamlit as st
 import requests
 from PIL import Image
@@ -311,6 +412,10 @@ from transformers import pipeline
 import folium
 from streamlit_folium import st_folium
 from streamlit_geolocation import streamlit_geolocation
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.ERROR)
 
 st.markdown("<h1 style='font-size: 20px;'>Welcome to Bird Identification Page</h1>", unsafe_allow_html=True)
 st.write("This page will help you identify bird species and find the best nearest bird observation hotspots")
@@ -378,29 +483,25 @@ if uploaded_file is not None:
             st.write("Failed to retrieve data from Wikipedia API")
             st.write(f"Error: {e}")
         except ValueError as e:
-            st.write("Failed to parse JSON response from Wikipedia API")
-            st.write(f"Error: {e}")
+            logging.error("Failed to parse JSON response from Wikipedia API", exc_info=True)
 
     st.write("⬇️ Please press the button to get your current location")
 
+    location = streamlit_geolocation()
 
+    if location and "latitude" in location and "longitude" in location:
+        lat, lon = location["latitude"], location["longitude"]
+        st.write(f"Your location is: {lat}, {lon}")
 
-location = streamlit_geolocation()
-if location and "latitude" in location and "longitude" in location:
-    lon = -43.5053818
-    lat = 172.5837443
-    lat, lon = location["latitude"], location["longitude"]
-    st.write(f"Your location is: {lat}, {lon}")
+        # Create a map centered at the current location
+        m = folium.Map(location=[lat, lon], zoom_start=12)
 
-    # Create a map centered at the current location
-    m = folium.Map(location=[lat, lon], zoom_start=12)
+        folium.Marker(
+            [lat, lon], 
+            popup="Current Location", 
+            icon=folium.Icon(color='red')  # Set marker color
+        ).add_to(m)
 
-    folium.Marker(
-        [lat, lon], 
-        popup="Current Location", 
-        icon=folium.Icon(color='red')  # Set marker color
-    ).add_to(m)
-
-    st_folium(m, width=600, height=300)
-else:
-    st.write("Please enable location services and press the button to get your current location.")
+        st_folium(m, width=600, height=300)
+    else:
+        st.write("Please enable location services and press the button to get your current location.")
