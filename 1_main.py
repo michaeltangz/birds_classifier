517f55d4f74e087f61f2befa90e23d778d7bb868
Michael TANG <tongmichael16@gmail.com>
2025-02-06 16:13:42 +1300

    update map live_classify.py

--- 1_main.py
+++ 1_main.py
@@ -10,7 +10,7 @@ st.title("HPAI Reporting Page")
 st.sidebar.success("Select a page to start")
 
 # add pic here
-st.image("h5n1.webp", use_column_width=True)
+st.image("h5n1.webp", use_container_width=True)
 
 st.markdown(
     """
